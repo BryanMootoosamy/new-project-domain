028e173e23a1824c99450e650e6059af96030eb2
Bryan Mootoosamy <mootoosamy.bryan@gmail.com>
2018-07-30 11:41:19 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/app.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <h1 class="title">Bryan's Domain</h1>
+        <div class="sp-container">
+            <div class="sp-content">
+                <div class="sp-globe"></div>
+                <h2 class="frame-1">BIENVENUE</h2>
+                <h2 class="frame-2">PRENEZ UN SIÈGE</h2>
+                <h2 class="frame-3">PENSEZ À VISITER MON PORTEFOLIO</h2>
+                <h2 class="frame-4">C'EST SYMPA</h2>
+                <h2 class="frame-5">
+                    <span class="span1">PASSIONNÉ,</span>
+                    <span class="span2">TRAVAILLEUR,</span>
+                    <span class="span3">MOTIVÉ.</span>
+                </h2>
+                <a class="sp-circle-link" href="https://github.com/BryanMootoosamy?tab=repositories">Bryan</a>
+            </div>
+    </div>
+    </body>
+</html>
